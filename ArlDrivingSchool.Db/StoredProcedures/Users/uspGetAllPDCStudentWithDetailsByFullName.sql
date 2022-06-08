0370d80b8571c47ab85b9feff39fa615bf5f542c
Joelito Deang <deangjoelito@gmail.com>
2022-06-08 13:50:54 +0800

    [TDC] Update modal in same name students section is not populating MOP and MOE. pdc students : same name table not showing date registered

--- ArlDrivingSchool.Db/StoredProcedures/Users/uspGetAllPDCStudentWithDetailsByFullName.sql
+++ ArlDrivingSchool.Db/StoredProcedures/Users/uspGetAllPDCStudentWithDetailsByFullName.sql
@@ -9,6 +9,15 @@ BEGIN
 		   ,ups.FBContact
 		   ,ups.Mobile
 		   ,la.StatusName [ACESStatus]
+		   ,o.OfficeId
+		   ,o.OfficeName
+		   ,em.EnrollmentModeId
+		   ,em.EnrollmentModeName
+		   ,u.UserId
+		   ,CONCAT(u.FirstName, ' ', u.LastName) [Staff] 
+		   ,pm.PaymentModeName
+		   ,t.TransactionId
+		   ,t.TransactionName
 		   ,ups.RestrictionId [RestrictionCode]
 		   ,ups.ATransmissionId
 		   ,ups.A1TransmissionId
@@ -20,17 +29,24 @@ BEGIN
 		   ,ups.DateCertified
 		   ,ups.CreatedBy
 		   ,ups.UpdatedBy
+		   ,ups.AuthenticatedBy
 
 		   ,pp.PDCPaymentId
 		   ,pp.PDCStudentId
 		   ,pp.TotalAmount
 		   ,pp.Payment [PaymentAmount]
 		   ,pp.Balance
-
+		   ,pp.PaymentModeId
+		   ,pm.PaymentModeName
 
 	FROM users.PDCStudent AS ups
 			INNER JOIN lookups.ACESStatus AS la ON la.ACESStatusId = ups.ACESStatusId
+			INNER JOIN lookups.EnrollmentMode AS em ON em.EnrollmentModeId = ups.EnrollmentModeId
+			LEFT JOIN lookups.Office AS o ON o.OfficeId = ups.OfficeId
+			LEFT JOIN users.[User] AS u ON u.UserId = ups.UserId
 			--INNER JOIN lookups.Restriction AS r ON r.RestrictionId = ups.RestrictionId
 			LEFT JOIN payments.PDCPayment AS pp ON pp.PDCStudentId = ups.PDCStudentId
+			LEFT JOIN lookups.PaymentMode AS pm ON pm.PaymentModeId = pp.PaymentModeId
+			LEFT JOIN lookups.[Transaction] AS t ON ups.TransactionId = t.TransactionId
 	WHERE ups.FullName LIKE '%'+@FullName+'%'
 END
--- ArlDrivingSchool.Db/StoredProcedures/Users/uspGetAllStudentWithDetailsByFullName.sql
+++ ArlDrivingSchool.Db/StoredProcedures/Users/uspGetAllStudentWithDetailsByFullName.sql
@@ -7,30 +7,43 @@ AS
 BEGIN
 	IF(@LastName IS NULL)
 		BEGIN
-			SELECT	us.StudentId
+				SELECT	us.StudentId
 		   ,us.FirstName
 		   ,us.LastName
 		   ,us.Email
 		   ,us.[Location]
 		   ,us.FBContact
 		   ,us.Mobile
+		   ,us.AcesSaveDate
 		   ,ls.StatusName [StudentStatus]
 		   ,la.StatusName [ACESStatus]
 		   ,lt.StatusName [TDCStatus]
+		   ,o.OfficeId
+		   ,o.OfficeName
+		   ,em.EnrollmentModeId
+		   ,em.EnrollmentModeName
+		   ,u.UserId
+		   ,CONCAT(u.FirstName, ' ', u.LastName) [Staff] 
+
 		   ,us.Remarks
 		   ,us.DateRegistered
 		   ,us.Certified
 		   ,us.DateCertified
-		   ,us.ClassType
 		   ,us.AuthenticatedBy
 		   ,us.CreatedBy
 		   ,us.UpdatedBy
+			,us.ClassType
+		   ,us.SessionEmail
+		   ,dss.StatusName [DriveSafeStatus]
+		   ,us.TextForm
 
 		   ,pp.PaymentId
 		   ,pp.StudentId
 		   ,pp.TotalAmount
 		   ,pp.Payment [PaymentAmount]
 		   ,pp.Balance
+		   ,pp.PaymentModeId
+		   ,pm.PaymentModeName
 
 		   ,sso.SessionOneId
 		   ,sso.StudentId
@@ -62,42 +75,62 @@ BEGIN
 		   ,ssth.BranchId
 		   ,(SELECT BranchName FROM [lookups].Branch WHERE BranchId = ssth.BranchId) AS BranchName
 
-			FROM users.Student AS us
-				INNER JOIN lookups.StudentStatus AS ls ON ls.StudentStatusId = us.StudentStatusId
-				INNER JOIN lookups.ACESStatus AS la ON la.ACESStatusId = us.ACESStatusId
-				INNER JOIN lookups.TDCStatus AS lt ON lt.TDCStatusId = us.TDCStatusId
-				LEFT JOIN payments.Payment AS pp ON pp.StudentId = us.StudentId
-				LEFT JOIN [sessions].SessionOne AS sso ON sso.StudentId = us.StudentId
-				LEFT JOIN [sessions].SessionTwo AS sstw ON sstw.StudentId = us.StudentId
-				LEFT JOIN [sessions].SessionThree AS ssth ON ssth.StudentId = us.StudentId
+	FROM users.Student AS us
+			INNER JOIN lookups.StudentStatus AS ls ON ls.StudentStatusId = us.StudentStatusId
+			INNER JOIN lookups.ACESStatus AS la ON la.ACESStatusId = us.ACESStatusId
+			INNER JOIN lookups.TDCStatus AS lt ON lt.TDCStatusId = us.TDCStatusId
+			INNER JOIN lookups.EnrollmentMode AS em ON em.EnrollmentModeId = us.EnrollmentModeId
+			LEFT JOIN lookups.Office AS o ON o.OfficeId = us.OfficeId
+			LEFT JOIN users.[User] AS u ON u.UserId = us.UserId
+			LEFT JOIN lookups.DriveSafeStatus AS dss ON dss.DriveSafeStatusId = us.DriveSafeStatusId
+			LEFT JOIN payments.Payment AS pp ON pp.StudentId = us.StudentId
+			LEFT JOIN lookups.PaymentMode AS pm ON pm.PaymentModeId = pp.PaymentModeId
+			LEFT JOIN [sessions].SessionOne AS sso ON sso.StudentId = us.StudentId
+			LEFT JOIN [sessions].SessionTwo AS sstw ON sstw.StudentId = us.StudentId
+			LEFT JOIN [sessions].SessionThree AS ssth ON ssth.StudentId = us.StudentId
 
 			WHERE (us.FirstName LIKE '%'+@FirstName+'%' OR us.LastName LIKE @FirstName)
 		
 		END
 	ELSE
 		BEGIN
-			SELECT	us.StudentId
+				SELECT	us.StudentId
 		   ,us.FirstName
 		   ,us.LastName
 		   ,us.Email
 		   ,us.[Location]
 		   ,us.FBContact
 		   ,us.Mobile
+		   ,us.AcesSaveDate
 		   ,ls.StatusName [StudentStatus]
 		   ,la.StatusName [ACESStatus]
 		   ,lt.StatusName [TDCStatus]
+		   ,o.OfficeId
+		   ,o.OfficeName
+		   ,em.EnrollmentModeId
+		   ,em.EnrollmentModeName
+		   ,u.UserId
+		   ,CONCAT(u.FirstName, ' ', u.LastName) [Staff] 
+
 		   ,us.Remarks
 		   ,us.DateRegistered
 		   ,us.Certified
 		   ,us.DateCertified
+		   ,us.AuthenticatedBy
 		   ,us.CreatedBy
 		   ,us.UpdatedBy
+			,us.ClassType
+		   ,us.SessionEmail
+		   ,dss.StatusName [DriveSafeStatus]
+		   ,us.TextForm
 
 		   ,pp.PaymentId
 		   ,pp.StudentId
 		   ,pp.TotalAmount
 		   ,pp.Payment [PaymentAmount]
 		   ,pp.Balance
+		   ,pp.PaymentModeId
+		   ,pm.PaymentModeName
 
 		   ,sso.SessionOneId
 		   ,sso.StudentId
@@ -129,14 +162,19 @@ BEGIN
 		   ,ssth.BranchId
 		   ,(SELECT BranchName FROM [lookups].Branch WHERE BranchId = ssth.BranchId) AS BranchName
 
-			FROM users.Student AS us
-				INNER JOIN lookups.StudentStatus AS ls ON ls.StudentStatusId = us.StudentStatusId
-				INNER JOIN lookups.ACESStatus AS la ON la.ACESStatusId = us.ACESStatusId
-				INNER JOIN lookups.TDCStatus AS lt ON lt.TDCStatusId = us.TDCStatusId
-				LEFT JOIN payments.Payment AS pp ON pp.StudentId = us.StudentId
-				LEFT JOIN [sessions].SessionOne AS sso ON sso.StudentId = us.StudentId
-				LEFT JOIN [sessions].SessionTwo AS sstw ON sstw.StudentId = us.StudentId
-				LEFT JOIN [sessions].SessionThree AS ssth ON ssth.StudentId = us.StudentId
+	FROM users.Student AS us
+			INNER JOIN lookups.StudentStatus AS ls ON ls.StudentStatusId = us.StudentStatusId
+			INNER JOIN lookups.ACESStatus AS la ON la.ACESStatusId = us.ACESStatusId
+			INNER JOIN lookups.TDCStatus AS lt ON lt.TDCStatusId = us.TDCStatusId
+			INNER JOIN lookups.EnrollmentMode AS em ON em.EnrollmentModeId = us.EnrollmentModeId
+			LEFT JOIN lookups.Office AS o ON o.OfficeId = us.OfficeId
+			LEFT JOIN users.[User] AS u ON u.UserId = us.UserId
+			LEFT JOIN lookups.DriveSafeStatus AS dss ON dss.DriveSafeStatusId = us.DriveSafeStatusId
+			LEFT JOIN payments.Payment AS pp ON pp.StudentId = us.StudentId
+			LEFT JOIN lookups.PaymentMode AS pm ON pm.PaymentModeId = pp.PaymentModeId
+			LEFT JOIN [sessions].SessionOne AS sso ON sso.StudentId = us.StudentId
+			LEFT JOIN [sessions].SessionTwo AS sstw ON sstw.StudentId = us.StudentId
+			LEFT JOIN [sessions].SessionThree AS ssth ON ssth.StudentId = us.StudentId
 
 			WHERE (us.FirstName LIKE '%'+@FirstName+'%' AND us.LastName LIKE '%'+@LastName+'%' )
 		END
